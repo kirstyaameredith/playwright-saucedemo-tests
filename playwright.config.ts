af18659e2468540451203aa8487e3045eaf5909d
kirstyaameredith <97108028+kirstyaameredith@users.noreply.github.com>
2025-12-31 13:49:39 +0000

    Allure reporting doc folder creation for smoke & regression

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
+const allureOutput = process.env.ALLURE_RESULTS_DIR;
+console.log('✅ Allure output folder from env:', allureOutput);
+
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
@@ -10,18 +13,15 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     baseURL: 'https://www.saucedemo.com',
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
   },
 
   reporter: [
     ['list'],
-    [
-      'allure-playwright',
-      {
-        outputFolder: process.env.ALLURE_RESULTS_DIR || 'allure-results',
-        detail: true,
-        suiteTitle: false
-      }
-    ]
+    ['allure-playwright', {
+      resultsDir: allureOutput || 'allure-results',
+      detail: true,
+      suiteTitle: false
+    }]
   ],
 });
